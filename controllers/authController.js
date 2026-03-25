bfdd6b92f991a0cde2b08854f13a52b2455a5b8e
chittrapriya5796 <krcpriya@gmail.com>
2026-03-25 16:45:07 +0530

    User registered saved in MongoDB

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,29 @@
+const User = require("../models/user.js")
+const bcrypt = require('bcrypt')
+
 const authController = {
     register: async(req, res) => {
         try {
-            res.status(201).json({messageL: "User Registered Successfully"})
+            //getr details from the request body
+            const {name, email, password} = req.body
+
+            //check if the user exists already
+            const existingUser = await User.findOne({email})
+
+            if(existingUser) {
+                return res.status(400).json({message: "User is alreay Exists"})
+            }
+
+            //encrypt the password
+            const hashedPassword = await bcrypt.hash(password, 10)
+
+            //create a new User
+            const newUser = new User ({ name, email,password: hashedPassword})
+
+            //save the user to the database
+            await newUser.save()
+
+            res.status(201).json({message: "User Registered Successfully"})
         } catch (error) {
             res.status(500).json({message: "Error registering user", error: error.message})
         }
