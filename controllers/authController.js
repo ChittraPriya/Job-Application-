0b13e2ded2b213e6d5b05eee8554fa1885930dfe
chittrapriya5796 <krcpriya@gmail.com>
2026-03-26 05:52:06 +0530

    Login Successfully added

--- controllers/authController.js
+++ controllers/authController.js
@@ -31,6 +31,29 @@ const authController = {
         } catch (error) {
             res.status(500).json({message: "Error registering user", error: error.message})
         }
+    },
+    login: async(req,res) => {
+        try {
+            const { email, password} = req.body;
+
+            //find the user by email
+            const user = await User.findOne({ email });
+
+            if(!user){
+                return res.status(400).json({message: 'Email does not Exists'})
+            }
+
+            //compare the password
+            const isMatch = await bcrypt.compare(password, user.password)
+
+            if(!isMatch) {
+                return res.status(400).json({message: 'Invalid Password'})
+            }
+
+            return res.status(200).json({message: 'Login Successfully'})
+        } catch (error) {
+             res.status(500).json({message: "Error Logging in", error: error.message})
+        }
     }
 }
 
